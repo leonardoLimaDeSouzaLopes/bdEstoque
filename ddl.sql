11f8fd0d52028dcc71cf224167e1cf3567ef98af
leonardoLimaDeSouzaLopes <91790685+leonardoLimaDeSouzaLopes@users.noreply.github.com>
2024-05-09 12:13:21 -0300

    Comecei os exercicios de Join
    Comecei os exercicios de Left Join, Right Join, Full Join e Cross Join, até agora só inseri 3 novos produtos

--- ddl.sql
+++ ddl.sql
@@ -8,50 +8,50 @@ USE bdEstoque
 
 CREATE TABLE tbCliente (
 	idCliente INT PRIMARY KEY IDENTITY(1,1)
-	, nomeCliente VARCHAR(200) NOT NULL
-	, cpfCliente CHAR(11) NOT NULL UNIQUE
+	, nomeCliente VARCHAR(200)
+	, cpfCliente CHAR(11) UNIQUE
 	, emailCliente VARCHAR(320)
 	, sexoCliente CHAR(1)
-	, dataNascimento DATETIME NOT NULL
+	, dataNascimento DATETIME
 )
 --DROP TABLE tbCliente
 
 CREATE TABLE tbVenda (
 	idVenda INT PRIMARY KEY IDENTITY(1,1)
-	, dataVenda DATETIME NOT NULL
-	, valorTotalVenda MONEY NOT NULL
-	, idCliente INT FOREIGN KEY REFERENCES tbCliente(idCliente) NOT NULL
+	, dataVenda DATETIME
+	, valorTotalVenda MONEY
+	, idCliente INT FOREIGN KEY REFERENCES tbCliente(idCliente)
 )
 --DROP TABLE tbVenda
 
 CREATE TABLE tbFabricante (
 	idFabricante INT PRIMARY KEY IDENTITY(1,1)
-	, nomeFabricante VARCHAR(100) NOT NULL
+	, nomeFabricante VARCHAR(100)
 )
 --DROP TABLE tbFabricante
 
 CREATE TABLE tbFornecedor (
 	idFornecedor INT PRIMARY KEY IDENTITY(1,1)
-	, nomeFornecedor VARCHAR(100) NOT NULL
-	, contatoFornecedor VARCHAR(50) NOT NULL
+	, nomeFornecedor VARCHAR(100)
+	, contatoFornecedor VARCHAR(50)
 )
 --DROP TABLE tbFornecedor
 
 CREATE TABLE tbProduto (
 	idProduto INT PRIMARY KEY IDENTITY(1,1)
-	, descricaoProduto VARCHAR(100) NOT NULL
-	, valorProduto MONEY NOT NULL
-	, quantidadeProduto INT NOT NULL
-	, idFabricante INT FOREIGN KEY REFERENCES tbFabricante(idFabricante) NOT NULL
-	, idFornecedor INT FOREIGN KEY REFERENCES tbFornecedor(idFornecedor) NOT NULL
+	, descricaoProduto VARCHAR(100)
+	, valorProduto MONEY
+	, quantidadeProduto INT
+	, idFabricante INT FOREIGN KEY REFERENCES tbFabricante(idFabricante)
+	, idFornecedor INT FOREIGN KEY REFERENCES tbFornecedor(idFornecedor)
 )
 --DROP TABLE tbProduto
 
 CREATE TABLE tbItensVenda (
 	idItensVenda INT PRIMARY KEY IDENTITY(1,1)
-	, idVenda INT FOREIGN KEY REFERENCES tbVenda(idVenda) NOT NULL
-	, idProduto INT FOREIGN KEY REFERENCES tbProduto(idProduto) NOT NULL
-	, quantidadeItensVenda INT NOT NULL
-	, subTotalItensVenda MONEY NOT NULL
+	, idVenda INT FOREIGN KEY REFERENCES tbVenda(idVenda)
+	, idProduto INT FOREIGN KEY REFERENCES tbProduto(idProduto)
+	, quantidadeItensVenda INT
+	, subTotalItensVenda MONEY
 )
 --DROP TABLE tbItensVenda
