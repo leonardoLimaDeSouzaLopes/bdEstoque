3ab38b7675526a5eb147a0289e484c92f9eff5eb
leonardoLimaDeSouzaLopes <leonardolslopes2007@gmail.com>
2024-05-16 06:10:14 -0300

    Minha dupla fez os primeiros exercicios
    O https://github.com/Triaguin fez os exercicios de 1 a 3 agora só falta eu deixa ele com os AS iguais ao resto e muda um poquinho para ficar no mesmo padrao dos outros

--- dql.sql
+++ dql.sql
@@ -51,6 +51,17 @@ SELECT SUM (tbVenda.valorTotalVenda) AS 'Vendas', tbCliente.nomeCliente AS 'Clie
 
 --EXERCICIOS JOINS
 
+--2)
+SELECT descricaoProduto AS 'Descrição produto', nomeFornecedor AS 'Nome fornecedores' FROM tbFornecedor F
+	INNER JOIN tbProduto P ON P.idFornecedor = F.idFornecedor
+
+--3)
+SELECT descricaoProduto AS 'Nome produto' , nomeFornecedor AS 'Nome fornecedores'FROM tbFornecedor f
+	LEFT JOIN tbProduto p ON p.idFornecedor = f.idFornecedor
+
+SELECT descricaoProduto AS 'Nome produto' , nomeFornecedor AS 'Nome fornecedores'FROM tbFornecedor f
+	RIGHT JOIN tbProduto p ON p.idFornecedor = f.idFornecedor
+
 --5)
 SELECT nomeFornecedor AS 'Fornecedor', descricaoProduto AS 'Produto' FROM tbFornecedor
 	FULL JOIN tbProduto ON tbFornecedor.idFornecedor = tbProduto.idFornecedor
